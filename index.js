62a9f5ac1e1c651930ee59b93575530f9d9e24de
bhnash74 <bhn774@gmail.com>
2021-08-18 22:13:39 -0500

    Added re-select loops

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ switch(dest) {
 
     break;
 }
-} console.log(pickDest());
+}
+//Agree or re-select destination
+let destAgree;
+while (destAgree!==1){ 
+let chosenDest=pickDest();
+   destAgree = parseInt(prompt(chosenDest+'  Is this a good choice? Pick "1" to keep or "2" to change'));
+  }
 
 //(5 points): As a user, I want a restaurant to be randomly selected for my day trip.
 function pickFood(){
@@ -37,12 +43,19 @@ switch(food) {
         return ("Your restaurant is Vegan");
 
     break;
-}
-} console.log(pickFood());
+} 
+  } 
+  //Agree or re-select restaurant
+  let foodAgree;
+  while (foodAgree!==1){ 
+  let chosenFood=pickFood();
+     foodAgree = parseInt(prompt(chosenFood+'  Is this a good choice? Pick "1" to keep or "2" to change'));
+    }
+ 
 //(5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
-function pickTransport(){
-let transport = Math.floor(Math.random() *4);
-switch(transport) {
+function pickTrans(){
+let trans = Math.floor(Math.random() *4);
+switch(trans) {
     case 0:
     return ("You will travel by airplane");
     break;
@@ -56,11 +69,17 @@ switch(transport) {
         return("You will travel by bus");
     break;
 }
-}console.log(pickTransport());
+}  
+  //Agree or re-select transportation
+  let transAgree;
+  while (transAgree!==1){ 
+  let chosenTrans=pickTrans();
+     transAgree = parseInt(prompt(chosenTrans+'  Is this a good choice? Pick "1" to keep or "2" to change'));
+    }
 //(5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
-function pickEntert(){
-let entert = Math.floor(Math.random() *4);
-switch(entert) {
+function pickEnt(){
+let ent = Math.floor(Math.random() *4);
+switch(ent) {
     case 0:
     return ("You will go sightseeing tour");
     break;
@@ -74,10 +93,20 @@ switch(entert) {
     return ("You will visit a museum");
     break;
 }
-} console.log(pickEntert());
+}  
+//Agree or re-select entertainment
+let entAgree;
+while (entAgree!==1){ 
+let chosenEnt=pickEnt();
+   entAgree = parseInt(prompt(chosenEnt+'  Is this a good choice? Pick "1" to keep or "2" to change'));
+  }
+
+  
 
 //(15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
 
+
 //(10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+
 //(10 points): As a user, I want to display my completed trip in the console.
 //(5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
