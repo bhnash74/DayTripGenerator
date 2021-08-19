b9534c5c085d623db6b2cb437e2c293da77d71be
bhnash74 <bhn774@gmail.com>
2021-08-18 19:51:40 -0500

    Created destination switch

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 "use strict"
-console.log("Hello there");
+//Day Trip generator
+//(5 points): As a user, I want a destination to be randomly selected for my day trip.
+let dest = Math.floor(Math.random() *4);
+switch(dest) {
+    case 0:
+    console.log("Your trip destination is Paris");
+    break;
+    case 1:
+        console.log("Your trip destination is London");
+    break;
+    case 2:
+        console.log("Your trip destination is Morocco");
+    break;
+    case 3:
+        console.log("Your trip destination is New York");
+
+    break;
+}
+//(5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+//(5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+//(5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+//(15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
+//(10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
+//(10 points): As a user, I want to display my completed trip in the console.
+//(5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
